84ab369b0063e780124d55f52d08572252c441d4
Chinmay kardpati <karodpatichinmay12@gmail.com>
2026-05-10 15:25:43 +0530

    Added a small dummy feature to demonstrate how features will be made enable and disable without even restarting microservices

--- order-service/order-service/src/main/java/com/chinmay/ecommerce/order_service/Controllers/OrderController.java
+++ order-service/order-service/src/main/java/com/chinmay/ecommerce/order_service/Controllers/OrderController.java
@@ -1,6 +1,7 @@
 package com.chinmay.ecommerce.order_service.Controllers;
 
 import com.chinmay.ecommerce.order_service.Client.InventoryOrdersFeignClient;
+import com.chinmay.ecommerce.order_service.Configs.FeaturesEnableConfig;
 import com.chinmay.ecommerce.order_service.Dto.OrderRequestDto;
 import com.chinmay.ecommerce.order_service.Service.OrderService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,6 +9,8 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.query.Order;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,14 +20,32 @@ import java.util.List;
 @RequestMapping("/core")
 @RequiredArgsConstructor
 @Slf4j
+// This annotation is used to refresh the configuration properties of the application at runtime without restarting the application. It is typically used in conjunction with Spring Cloud Config to allow dynamic updates to configuration properties. When a configuration change is detected,
+// the beans annotated with @RefreshScope will be reloaded with the new configuration values.
+@RefreshScope
 public class OrderController {
     private final OrderService ordersService;
+    private final FeaturesEnableConfig featuresEnableConfig;
+
+    @Value("${my.variable}")
+    private String myVariable;
 
     @GetMapping("/helloOrders")
     // Here we are fetching the data that is passed from API Gateway to the downstream services
-    public String helloOrders(@RequestHeader("X-User-Id") String userId) {
+//    public String helloOrders(@RequestHeader("X-User-Id") String userId) {
+//        log.info("Received request to /helloOrders endpoint");
+//        return "Hello from Orders Service , User ID: " + userId;
+//     }
+
+    public String helloOrders() {
         log.info("Received request to /helloOrders endpoint");
-        return "Hello from Orders Service , User ID: " + userId;
+
+        if(featuresEnableConfig.isUserTrackingEnabled()){
+            return "User Tracking Enabled , myVariable: " + myVariable;
+        }
+        else{
+            return "User Tracking Disabled , myVariable: " + myVariable;
+        }
     }
 
     @PostMapping("/create-order")
